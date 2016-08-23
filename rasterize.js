826aaf4e106d1a83fc6bf55bd4e0d8aeccf1b071
maxdonetsk <max.donetsk@mail.ru>
2016-08-23 15:04:00 +0300

    initial commit

---
+++ rasterize.js
@@ -0,0 +1,175 @@
+"use strict";
+var page = require('webpage').create(),
+    system = require('system'),
+    address, output, size;
+
+    address = 'http://lang.makesocial.net';
+    output = 'test.pdf';
+    page.viewportSize = { width: 600, height: 600 };
+    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
+        size = system.args[3].split('*');
+        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
+                                   : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
+    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
+    size = system.args[3].split('*');
+    if (size.length === 2) {
+        pageWidth = parseInt(size[0], 10);
+        pageHeight = parseInt(size[1], 10);
+            page.viewportSize = { width: pageWidth, height: pageHeight };                                     
+            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
+    } else {
+        console.log("size:", system.args[3]);
+        pageWidth = parseInt(system.args[3], 10);
+        pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
+        console.log ("pageHeight:",pageHeight);
+        page.viewportSize = { width: pageWidth, height: pageHeight };
+    }
+    }
+    if (system.args.length > 4) {
+        page.zoomFactor = system.args[4];
+    }
+    
+    
+    
+    function waitFor(testFx, onReady, timeOutMillis) {
+        var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+    condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+            // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                } else {
+                // Condition fulfilled (timeout and/or condition is 'true')
+                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 3000); //< repeat check every 250ms
+    };
+    
+    
+    
+    page.open(address, function (status) {
+        // Check for page load success
+        if (status !== "success") {
+            console.log("Unable to access network");
+        } else {
+            page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function () {
+        page.evaluateAsync(function(){
+                    var finishedCall = false;
+                $.ajax({
+                    url: 'http://lang.makesocial.net/index.php?r=pages/pages/page1&lang=en&disc=4',
+                    data: 'languageChoiseSelect=4',
+                    type: 'GET',
+                    success: function (data) {
+                        console.log('Yes!');
+                        console.log(data);
+                            finishedCall = true;
+                    },
+                    });
+            });
+            waitFor(function check(){
+                    return page.evaluate(function(){
+                return finishedCall;
+                });
+            }, function onReady(){
+                    phantom.exit();  
+            }, 10000); // 10 seconds maximum timeout
+            });
+    }
+    });
+        };
+    </script>              <script>
+        var __run = function() {
+            "use strict";
+var page = require('webpage').create(),
+    system = require('system'),
+    address, output, size;
+
+    address = 'http://lang.makesocial.net';
+    output = 'test.pdf';
+    page.viewportSize = { width: 600, height: 600 };
+    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
+        size = system.args[3].split('*');
+        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
+                                   : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
+    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
+    size = system.args[3].split('*');
+    if (size.length === 2) {
+        pageWidth = parseInt(size[0], 10);
+        pageHeight = parseInt(size[1], 10);
+            page.viewportSize = { width: pageWidth, height: pageHeight };                                     
+            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
+    } else {
+        console.log("size:", system.args[3]);
+        pageWidth = parseInt(system.args[3], 10);
+        pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
+        console.log ("pageHeight:",pageHeight);
+        page.viewportSize = { width: pageWidth, height: pageHeight };
+    }
+    }
+    if (system.args.length > 4) {
+        page.zoomFactor = system.args[4];
+    }
+    
+    
+    
+    function waitFor(testFx, onReady, timeOutMillis) {
+        var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+    condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+            // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                } else {
+                // Condition fulfilled (timeout and/or condition is 'true')
+                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 3000); //< repeat check every 250ms
+    };
+    
+    
+    
+    page.open(address, function (status) {
+        var finishedCall = false;
+        // Check for page load success
+        if (status !== "success") {
+            console.log("Unable to access network");
+        } else {
+            page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function () {
+        page.evaluateAsync(function(){
+                $.ajax({
+                    url: 'http://lang.makesocial.net/index.php?r=pages/pages/page1&lang=en&disc=4',
+                    data: 'languageChoiseSelect=4',
+                    type: 'GET',
+                    success: function (data) {
+                        console.log('Yes!');
+                        console.log(data);
+                            finishedCall = true;
+                    },
+                    });
+            });
+            waitFor(function check(){
+                    return page.evaluate(function(){
+                return finishedCall;
+                });
+            }, function onReady(){
+                    phantom.exit();  
+            }, 10000); // 10 seconds maximum timeout
+            });
+    }
+    });
