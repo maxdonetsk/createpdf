1fa35a4ad52b64d6ca9b39b3f210a6153dadcf7c
maxdonetsk <max.donetsk@mail.ru>
2016-08-23 16:32:41 +0300

    logic changed for testFx

--- rasterize.js
+++ rasterize.js
@@ -1,77 +1,81 @@
 "use strict";
 var page = require('webpage').create(),
-    system = require('system'),
-    address, output, size;
+        system = require('system'),
+        address, output, size;
 
-    address = 'http://makesocial.net';
-    output = 'test.pdf';
-    page.viewportSize = { width: 600, height: 600 };
-    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-        size = system.args[3].split('*');
-        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-                                           : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
-    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
-        size = system.args[3].split('*');
-        if (size.length === 2) {
-            pageWidth = parseInt(size[0], 10);
-            pageHeight = parseInt(size[1], 10);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
-        } else {
-            console.log("size:", system.args[3]);
-            pageWidth = parseInt(system.args[3], 10);
-            pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
-            console.log ("pageHeight:",pageHeight);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-        }
+address = 'http://makesocial.net';
+output = 'test.pdf';
+page.viewportSize = {width: 600, height: 600};
+if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
+    size = system.args[3].split('*');
+    page.paperSize = size.length === 2 ? {width: size[0], height: size[1], margin: '0px'}
+    : {format: system.args[3], orientation: 'portrait', margin: '1cm'};
+} else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
+    size = system.args[3].split('*');
+    if (size.length === 2) {
+        pageWidth = parseInt(size[0], 10);
+        pageHeight = parseInt(size[1], 10);
+        page.viewportSize = {width: pageWidth, height: pageHeight};
+        page.clipRect = {top: 0, left: 0, width: pageWidth, height: pageHeight};
+    } else {
+        console.log("size:", system.args[3]);
+        pageWidth = parseInt(system.args[3], 10);
+        pageHeight = parseInt(pageWidth * 3 / 4, 10); // it's as good an assumption as any
+        console.log("pageHeight:", pageHeight);
+        page.viewportSize = {width: pageWidth, height: pageHeight};
     }
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
-    }
+}
+if (system.args.length > 4) {
+    page.zoomFactor = system.args[4];
+}
 
 
-    function waitFor(testFx, onReady, timeOutMillis) {
+function waitFor(testFx, onReady, timeOutMillis) {
     var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-            } else {
-                if(!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
+            start = new Date().getTime(),
+            condition = false,
+            interval = setInterval(function () {
+                if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
+                    // If not time-out yet and condition not yet fulfilled
+                    condition = (typeof (testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
                 } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
+                    if (!condition) {
+                        // If condition still not fulfilled (timeout but condition is 'false')
+                        console.log("'waitFor()' timeout");
+                        phantom.exit(1);
+                    } else {
+                        // Condition fulfilled (timeout and/or condition is 'true')
+                        console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                        typeof (onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                        clearInterval(interval); //< Stop this interval
+                    }
                 }
-            }
-        }, 250); //< repeat check every 250ms
-};
+            }, 250); //< repeat check every 250ms
+}
 
 page.open(address, function (status) {
-	var isClicked = false;
+    var isClicked = false;
     if (status !== "success") {
         console.log("Unable to access network");
     } else {
         page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function () {
-            waitFor(function() {
+            waitFor(function () {
                 // Check in the page if a specific element is now visible
-                var isVisible = page.evaluate(function(isClicked) {
-                	if (isClicked === false) {
-            			$('.nw-page-2').click();
-            			isClicked = true;
-            		};
-                    return $("#nw-registration").is(":visible");
-                }, isClicked);
-            }, function() {
-               console.log("The Registration button should be visible now.");
-               page.render(output);
-               phantom.exit();
+                if (isClicked === false) {
+                    page.evaluate(function () {
+                        $('.nw-page-2').click();
+                    });
+                    isClicked = true;
+                    return false;
+                } else {
+                    page.evaluate(function () {
+                        return $("#nw-registration").is(":visible");
+                    });
+                }
+            }, function () {
+                console.log("The Registration button should be visible now.");
+                page.render(output);
+                phantom.exit();
             });
         });
     }
