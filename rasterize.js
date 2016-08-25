e16b1373c9063c53258113916e10a6983f71dc06
maxdonetsk <max.donetsk@mail.ru>
2016-08-25 10:59:03 +0300

    embedding waitFor into page.open, removing timeout

--- rasterize.js
+++ rasterize.js
@@ -29,45 +29,23 @@ if (system.args.length > 4) {
     page.zoomFactor = system.args[4];
 }
 
-
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
-            start = new Date().getTime(),
-            condition = false,
-            interval = setInterval(function () {
-                if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
-                    // If not time-out yet and condition not yet fulfilled
-                    condition = (typeof (testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-                } else {
-                    if (!condition) {
-                        // If condition still not fulfilled (timeout but condition is 'false')
-                        console.log("'waitFor()' timeout");
-                        phantom.exit(1);
-                    } else {
-                        // Condition fulfilled (timeout and/or condition is 'true')
-                        console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                        typeof (onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                        clearInterval(interval); //< Stop this interval
-                    }
-                }
-            }, 250); //< repeat check every 250ms
-}
-
 page.open(address, function (status) {
-    var isClicked = false;
     if (status !== "success") {
         console.log("Unable to access network");
     } else {
-//        page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function () {
-            waitFor(function () {
-                return page.evaluate(function () {
-                    return $('.fa.fa-hand-paper-o').is(':visible');
-                });
-            }, function () {
-                console.log("The Registration button should be visible now.");
-                page.render(output);
-                phantom.exit();
-            });
-//        });
+        var condition = false,
+                interval = setInterval(function () {
+                    if (!condition) {
+                        console.log('Failed.');
+                        return page.evaluate(function () {
+                            return $('.fa.fa-hand-paper-o').is(':visible');
+                        });
+                    } else {
+                        console.log('Success!');
+                        clearInterval(interval);
+                        page.render(output);
+                        phantom.exit();
+                    }
+                }, 250);
     }
 });
