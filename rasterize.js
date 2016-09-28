b29bbfe68efe550b5f94e81e9844e512db382a9b
maxdonetsk <max.donetsk@mail.ru>
2016-09-28 10:57:27 +0300

    use original code from Server with changed logic

--- rasterize.js
+++ rasterize.js
@@ -3,6 +3,79 @@ var page = require('webpage').create(),
         address, output, size,
         organizationName, authToken, domain;
 
+if (system.args.length < 3 || system.args.length > 7) {
+    console.log('Usage: rasterize.js URL filename paperwidth*paperheight|paperformat organizationName authToken domain');
+    console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
+    phantom.exit(1);
+} else {
+    address = system.args[1];
+    output = system.args[2];
+    page.viewportSize = {width: 700, height: 600};
+    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
+        size = system.args[3].split('*');
+        page.paperSize = size.length === 2 ? {width: size[0], height: size[1], margin: '0px'}
+        : {format: system.args[3], orientation: 'portrait', margin: '1cm'};
+    }
+    organizationName = system.args[4];
+    authToken = system.args[5];
+    domain = system.args[6];
+
+    phantom.addCookie({
+        'name': 'DEFAULT_ORG_NAME', /* required property */
+        'value': organizationName, /* required property */
+        'domain': domain,
+        'path': '/', /* required property */
+        'httponly': false,
+        'secure': false,
+        'expires': (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
+    });
+    phantom.addCookie({
+        'name': 'auth-token', /* required property */
+        'value': authToken, /* required property */
+        'domain': "intendu.com",
+        'path': '/', /* required property */
+        'httponly': false,
+        'secure': false,
+        'expires': (new Date()).getTime() + (1000 * 60 * 60)   /* <-- expires in 1 hour */
+    });
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+            phantom.exit();
+        } else {
+            var condition = false,
+                    interval = setInterval(function () {
+                        if (!condition) {
+                            condition = page.evaluate(function () {
+                                return $('#allChartsRendered').is(':visible');
+                            });
+                        } else {
+                            clearInterval(interval);
+                            page.render(output);
+//                            page.render('/dev/stdout', {format: 'pdf'})
+                            phantom.exit();
+                        }
+                    }, 250);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+var page = require('webpage').create(),
+        system = require('system'),
+        address, output, size,
+        organizationName, authToken, domain;
+
 page.onResourceError = function (resourceError) {
     console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
     console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
